982a2a8835e690a6b01685828f5a402f0d5d37ea
«Tehnik762» <yastroitel@gmail.com>
2023-11-01 11:18:45 +0100

    Problem 242 added

---
+++ easy/242.ValidAnagram.py
@@ -0,0 +1,12 @@
+# Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+#
+# An Anagram is a word or phrase formed by rearranging the letters of a
+# different word or phrase, typically using all the original letters exactly once.
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s =list(s)
+        s.sort()
+        t = list(t)
+        t.sort()
+        return s == t
